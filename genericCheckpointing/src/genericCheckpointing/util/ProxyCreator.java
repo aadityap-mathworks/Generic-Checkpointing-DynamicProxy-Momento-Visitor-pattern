f503297b40f081bfdd7647893322b5fd244ab311
aadya <apatil19@binghamton.edu>
2018-11-29 01:14:08 -0500

    added ProxyCreator and SerializableObject classes

---
+++ genericCheckpointing/src/genericCheckpointing/util/ProxyCreator.java
@@ -0,0 +1,25 @@
+package genericCheckpointing.util;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+import genericCheckpointing.server.StoreRestoreI;
+
+public class ProxyCreator {
+
+	public ProxyCreator() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public StoreRestoreI createProxy(Class<?>[] interfaceArray, InvocationHandler handler){
+
+		StoreRestoreI  serDeserObj =
+	            (StoreRestoreI)
+	            Proxy.newProxyInstance(
+	                                   getClass().getClassLoader(),
+	                                   interfaceArray,
+	                                   handler
+	                                   );
+		return serDeserObj;
+	}
+}
