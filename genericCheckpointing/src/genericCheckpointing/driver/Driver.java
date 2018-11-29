ac686821081f30188203e6b79f7dc49e270d125b
aadya <apatil19@binghamton.edu>
2018-11-29 06:36:18 -0500

    added SerStrategy interface and empty StoreRestoreHandler

--- genericCheckpointing/src/genericCheckpointing/driver/Driver.java
+++ genericCheckpointing/src/genericCheckpointing/driver/Driver.java
@@ -1,5 +1,14 @@
 package genericCheckpointing.driver;
 
+import genericCheckpointing.server.RestoreI;
+import genericCheckpointing.server.StoreI;
+import genericCheckpointing.server.StoreRestoreI;
+import genericCheckpointing.util.MyAllTypesFirst;
+import genericCheckpointing.util.MyAllTypesSecond;
+import genericCheckpointing.util.ProxyCreator;
+import genericCheckpointing.util.SerializableObject;
+import genericCheckpointing.xmlStoreRestore.StoreRestoreHandler;
+
 /**
  * @author Aaditya Sakharam Patil
  *
@@ -24,9 +33,32 @@ public class Driver {
 				System.exit(1);
 			}
 			
+			String mode = args[0];
+			int NUM_OF_OBJECTS = Integer.parseInt(args[1]);
+			String fileName = args[2];
 			
 			
 			
+			// FIXME: read the value of checkpointFile from the command line
+			
+			ProxyCreator pc = new ProxyCreator();
+			
+			// create an instance of StoreRestoreHandler (which implements
+			// the InvocationHandler
+			
+			// create a proxy
+			StoreRestoreI cpointRef = (StoreRestoreI) pc.createProxy(
+										 new Class[] {
+										     StoreI.class, RestoreI.class
+										 }, 
+										 new StoreRestoreHandler()
+										 );
+				
+			// FIXME: invoke a method on the handler instance to set the file name for checkpointFile and open the file
+
+			MyAllTypesFirst myFirst;
+			MyAllTypesSecond  mySecond;
+
 		}
 		catch (Exception e) {
 			e.printStackTrace();
---
+++ genericCheckpointing/src/genericCheckpointing/xmlStoreRestore/StoreRestoreHandler.java
@@ -0,0 +1,18 @@
+package genericCheckpointing.xmlStoreRestore;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class StoreRestoreHandler implements InvocationHandler{
+
+	public StoreRestoreHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
